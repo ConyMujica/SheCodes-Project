41e2c831ce325e3a1eaebbc9ec792f1bc893aa03
ConyMujica <71676736+ConyMujica@users.noreply.github.com>
2023-03-09 10:19:57 +0100

    Title changed to project name

---
+++ script.js
@@ -0,0 +1,65 @@
+let now = new Date();
+let p = document.querySelector("#dayTime");
+
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+p.innerHTML = `${day} ${hour}:${minutes} `;
+
+function newSearch(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function search(city) {
+  let apiKey = "c4ea742bb9bd639a1346b4ed50c2d997";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(newSearch);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "c4ea742bb9bd639a1346b4ed50c2d997";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  console.log(apiUrl);
+  axios.get(apiUrl).then(newSearch);
+}
+
+function searchCurrentCity(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchform = document.querySelector("form");
+searchform.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", searchCurrentCity);
+
+search("London");
